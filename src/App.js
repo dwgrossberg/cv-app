58c684791bc524268bf7c92b5ca4a7fc4c2911bd
Daniel <dwgrossberg@gmail.com>
2022-09-17 18:49:37 -0400

    Setup Footer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CVInput from "./components/CVInput";
 import CVOutput from "./components/CVOutput";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import "./styles/App.css";
 
 class App extends Component {
@@ -14,6 +15,7 @@ class App extends Component {
         <Header />
         <CVInput />
         <CVOutput />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import "../styles/Footer.css";
+import gitHubIcon from "../assets/github.png";
+
+class Footer extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="Footer">
+        <div className="made-by">
+          <a
+            href="https://www.theodinproject.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <p>The Odin Project</p>
+          </a>
+          <a href="https://github.com/dwgrossberg">
+            <img id="github-icon" alt="github-icon" src={gitHubIcon} />
+          </a>
+          <p>Made by Dan Grossberg</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
